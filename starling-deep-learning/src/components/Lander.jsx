a1a7299aa03b157b1be8c891d9c0c760544af13f
CodyPellom <paulpellom@gmail.com>
2018-11-02 10:13:56 -0400

    first

---
+++ starling-deep-learning/src/components/Lander.jsx
@@ -0,0 +1,20 @@
+
+import React, { Component } from 'react'
+import Styled from 'styled-components';
+
+const LanderBackground = Styled.div`
+background-color: grey;
+`
+
+export default class componentName extends Component {
+    render() {
+        return (
+            <LanderBackground>
+                
+   
+   <img src="https://i.imgur.com/Bcfb0vk.png" alt="logo"/>
+   
+ </LanderBackground>
+        )
+    }
+}
