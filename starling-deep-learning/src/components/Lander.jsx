0d5399cf0d219bb46d1a12de8817bc746d3026c8
CodyPellom <paulpellom@gmail.com>
2018-11-02 12:19:56 -0400

    transitional effects added to lander

--- starling-deep-learning/src/components/Lander.jsx
+++ starling-deep-learning/src/components/Lander.jsx
@@ -1,9 +1,11 @@
 
 import React, { Component } from 'react'
 import Styled from 'styled-components';
+import Bounce from 'react-reveal/Bounce';
+import Fade from 'react-reveal/Fade';
 
 const LanderStyler = Styled.div`
-background-color: grey;
+background-color: rgb(42, 59, 79);
 font-family: 'Raleway';
 color: white; 
 #Slogan {
@@ -24,10 +26,13 @@ export default class componentName extends Component {
         return (
             <LanderStyler>
                 
-   
+   <Bounce>
    <img id="Starling-Rough-Logo" src="https://i.imgur.com/HmyKxRV.gif" alt="logo"/>
+   </Bounce>
+   <Fade>
    <h1 id="Slogan-Top">Starling Deep Learning</h1>
    <h4 id="Slogan">Living better. Now.</h4>
+   </Fade>
  </LanderStyler>
         )
     }
