35600b599e6b6ffd7ef1cbe93b995700fe7a0e46
CodyPellom <paulpellom@gmail.com>
2018-11-02 12:11:08 -0400

    depends

--- starling-deep-learning/src/components/Lander.jsx
+++ starling-deep-learning/src/components/Lander.jsx
@@ -2,19 +2,33 @@
 import React, { Component } from 'react'
 import Styled from 'styled-components';
 
-const LanderBackground = Styled.div`
+const LanderStyler = Styled.div`
 background-color: grey;
+font-family: 'Raleway';
+color: white; 
+#Slogan {
+    padding-bottom: 200px;
+    font-size: 30px;
+}
+#Slogan-Top {
+    font-size: 50px;
+}
+#Starling-Rough-Logo {
+    height: 250px;
+    width: 250px;
+}
 `
 
 export default class componentName extends Component {
     render() {
         return (
-            <LanderBackground>
+            <LanderStyler>
                 
    
-   <img src="https://i.imgur.com/Bcfb0vk.png" alt="logo"/>
-   
- </LanderBackground>
+   <img id="Starling-Rough-Logo" src="https://i.imgur.com/HmyKxRV.gif" alt="logo"/>
+   <h1 id="Slogan-Top">Starling Deep Learning</h1>
+   <h4 id="Slogan">Living better. Now.</h4>
+ </LanderStyler>
         )
     }
 }
